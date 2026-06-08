f0d24ab2dc60496bd8128a62e44f757ad636e45d
sona5959 <sona.professional59@gmail.com>
2026-06-08 08:49:05 +0800

    Tour and room renting project

--- app.js
+++ app.js
@@ -50,6 +50,8 @@ main().then(()=>
 async function main()
 {
     await mongoose.connect(dbUrl);
+
+    // await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust")
 }
 
 app.set("views", "ejs");
